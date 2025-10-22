f78ff8c618896a466941f64d68d2467a4752f955
samkelemen <samkelemen17@gmail.com>
2025-10-22 18:45:23 -0500

    Implement inventoryManager.py and inventory.py

--- src/inventory.py
+++ src/inventory.py
@@ -1,5 +1,9 @@
+from item import Item
+
 class Inventory:
     def __init__(self):
-        self.inventory = None
+        self.inventory: Dict[str, Item] = {}
 
-    
+    def addItem(self, new_item: Item):
+        self.inventory[Item.name] = new_item
+
--- src/inventoryManager.py
+++ src/inventoryManager.py
@@ -1,18 +1,37 @@
+from inventory import Inventory
+from item import Item
+
 class InventoryManager:
-    def __init__(self, inventory):
+    def __init__(self, inventory: Inventory):
         self.inventory = inventory
     
         def listOutOfStock(self):
-            pass
+            out_of_stock: list = []
+            for item in inventory:
+                if not item.status:
+                    out_of_stock.append(item)
+            return out_of_stock
 
-        def addItem(self, name:str, quantity:int, status:bool):
-            pass
+        def addItem(self, name: str, quantity: int, status: bool):
+            if name in self.inventory:
+                self.changeItemQuantity(name, quantity)
+                return 
+
+            newItem: Item = Item(name, quantity, status)
+            inventory.addItem(item)
 
         def changeItemStatus(self, name:str, status:bool):
-            pass
+            if name not in self.inventory:
+                return
+
+            inventory[name].status = status
+
+        def changeItemQuantity(self, name, quantity):
+            if name not in self.inventory:
+                return
+
+            inventory[name].quantity += quantity
 
         def generateGroceryList(): 
             pass
 
-        def getItem(name:str):
-            pass
